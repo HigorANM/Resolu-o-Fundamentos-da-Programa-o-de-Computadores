3331651e26ec5d343248a7d7ca141aec2882c6cc
HigorANM <54604776+HigorANM@users.noreply.github.com>
2019-11-14 14:40:52 -0300

    Update Questao10.java

--- Questao10.java
+++ Questao10.java
@@ -5,21 +5,22 @@ import javax.swing.JOptionPane;
 public class questao10 {
 
     public static void main(String[] args) {
-        int impar = 0, par = 0, numero;
+        int primo = 0, par = 0, numero;
         int c = 0;
         while (c != 10) {
             numero = Integer.parseInt(JOptionPane.showInputDialog("Informe um numero: "));
 
             if ((numero % 2) == 0) {
                 par += numero;
-            } else {
-                impar += numero;
+            } 
+            if(numero % 1 == 0){
+                primo += numero;
             }
 
             c++;
 
         }
         JOptionPane.showMessageDialog(null, "a soma dos números pares é: "+par);
-        JOptionPane.showMessageDialog(null, "a soma dos números impares é: "+impar);
+        JOptionPane.showMessageDialog(null, "a soma dos números impares é: "+primo);
     }
 }
